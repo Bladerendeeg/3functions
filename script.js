63f61db840e062fec90bb7ca6e04156f76e94913
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-28 13:14:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function numbers1(n1, n2) {
-    return n1 + n2;
+    const n1kwad = n1 * n1;
+    const n2kwad = n2 * n2;
+    const n3optel = n1kwad + n2kwad;
+    const n4kwad = n3optel * n3optel;
+    return n4kwad;
 }
 console.log(numbers1(2, 3));
 
 const numbers2 = function (n1, n2) {
-    return n1 + n2;
+    const n1kwad = n1 * n1;
+    const n2kwad = n2 * n2;
+    const n3optel = n1kwad + n2kwad;
+    const n4kwad = n3optel * n3optel;
+    return n4kwad;
 }
-console.log(numbers2(1, 3));
+console.log(numbers2(2, 2));
 
 const numbers3 = (n1, n2) => {
-    return n1 + n2;
+    const n1kwad = n1 * n1;
+    const n2kwad = n2 * n2;
+    const n3optel = n1kwad + n2kwad;
+    const n4kwad = n3optel * n3optel;
+    return n4kwad;
 }
 console.log(numbers3(3, 3));
