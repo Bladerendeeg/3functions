3c1db7867caa44ef99d149a96304281b7f004429
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-28 13:09:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function numbers1(n1, n2) {
+    return n1 + n2;
+}
+console.log(numbers1(2, 3));
+
+const numbers2 = function (n1, n2) {
+    return n1 + n2;
+}
+console.log(numbers2(1, 3));
+
+const numbers3 = (n1, n2) => {
+    return n1 + n2;
+}
+console.log(numbers3(3, 3));
